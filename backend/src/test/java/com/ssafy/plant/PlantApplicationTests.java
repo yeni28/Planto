724dec89b6a3f57f673d6785bf97da82d3e5ea73
Lucas0105 <dnjswo105@gmail.com>
2023-01-17 15:53:21 +0900

    [BE] : init spring boot

---
+++ backend/src/test/java/com/ssafy/plant/PlantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.plant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
