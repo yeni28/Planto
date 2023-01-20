4e175ca910e18c398001e4f765b8768f974d74b6
Lucas0105 <dnjswo105@gmail.com>
2023-01-20 15:11:19 +0900

    [BE] feat : mosquitto subscribe

--- backend/src/test/java/com/ssafy/plant/PlantApplicationTests.java
+++ backend/src/test/java/com/ssafy/plant/PlantApplicationTests.java
@@ -3,11 +3,11 @@ package com.ssafy.plant;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = PlantApplication.class)
 class PlantApplicationTests {
 
 	@Test
 	void contextLoads() {
-	}
 
+	}
 }
