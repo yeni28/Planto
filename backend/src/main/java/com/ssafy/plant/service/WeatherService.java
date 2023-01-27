0b503a8fc0ebd5e760ab48fa5d295006894518dc
Lucas0105 <dnjswo105@gmail.com>
2023-01-27 17:55:32 +0900

    S08P12C202-107 [BE] feat : 날씨 api 전처리 진행 중

--- backend/src/main/java/com/ssafy/plant/service/WeatherService.java
+++ backend/src/main/java/com/ssafy/plant/service/WeatherService.java
@@ -9,6 +9,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -24,12 +25,12 @@ public class WeatherService {
             StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst"); /*URL*/
             urlBuilder.append("?" + URLEncoder.encode("serviceKey", "UTF-8") + secretKey); /*Service Key*/
             urlBuilder.append("&" + URLEncoder.encode("pageNo", "UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지번호*/
-            urlBuilder.append("&" + URLEncoder.encode("numOfRows", "UTF-8") + "=" + URLEncoder.encode("1000", "UTF-8")); /*한 페이지 결과 수*/
+            urlBuilder.append("&" + URLEncoder.encode("numOfRows", "UTF-8") + "=" + URLEncoder.encode("500", "UTF-8")); /*한 페이지 결과 수*/
             urlBuilder.append("&" + URLEncoder.encode("dataType", "UTF-8") + "=" + URLEncoder.encode("JSON", "UTF-8")); /*요청자료형식(XML/JSON) Default: XML*/
-            urlBuilder.append("&" + URLEncoder.encode("base_date", "UTF-8") + "=" + URLEncoder.encode("20230125", "UTF-8")); /*‘21년 6월 28일발표*/
+            urlBuilder.append("&" + URLEncoder.encode("base_date", "UTF-8") + "=" + URLEncoder.encode("20230127", "UTF-8")); /*‘21년 6월 28일발표*/
             urlBuilder.append("&" + URLEncoder.encode("base_time", "UTF-8") + "=" + URLEncoder.encode("0500", "UTF-8")); /*05시 발표*/
-            urlBuilder.append("&" + URLEncoder.encode("nx", "UTF-8") + "=" + URLEncoder.encode("55", "UTF-8")); /*예보지점의 X 좌표값*/
-            urlBuilder.append("&" + URLEncoder.encode("ny", "UTF-8") + "=" + URLEncoder.encode("127", "UTF-8")); /*예보지점의 Y 좌표값*/
+            urlBuilder.append("&" + URLEncoder.encode("nx", "UTF-8") + "=" + URLEncoder.encode("35", "UTF-8")); /*예보지점의 X 좌표값*/
+            urlBuilder.append("&" + URLEncoder.encode("ny", "UTF-8") + "=" + URLEncoder.encode("126", "UTF-8")); /*예보지점의 Y 좌표값*/
             URL url = new URL(urlBuilder.toString());
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("GET");
@@ -49,14 +50,17 @@ public class WeatherService {
             rd.close();
             conn.disconnect();
 
-//            Map<String, String> map = new HashMap<>();
-//
-//            ObjectMapper objectMapper = new ObjectMapper();
-//            map = objectMapper.readValue(sb.toString(), map.getClass());
-            System.out.println(sb.toString());
-//            System.out.println(map.getClass().getName());
-//            System.out.println(map.get("response"));
+            Map<String, Object> map = new HashMap<>();
 
+            ObjectMapper objectMapper = new ObjectMapper();
+            map = objectMapper.readValue(sb.toString(), map.getClass());
+            map = (Map<String, Object>) map.get("response");
+            map = (Map<String, Object>) map.get("body");
+            map = (Map<String, Object>) map.get("items");
+            ArrayList<Object> res = (ArrayList<Object>) map.get("item");
+            for (Object weather:res) {
+                System.out.println(weather);
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
