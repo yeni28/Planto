e5055f48cdcdcb49d1c6d7bad01e984226eb26f7
Lucas0105 <dnjswo105@gmail.com>
2023-02-01 09:38:52 +0900

    [BE] feat : setting redis expiration

--- backend/src/main/java/com/ssafy/plant/config/cache/CacheConfig.java
+++ backend/src/main/java/com/ssafy/plant/config/cache/CacheConfig.java
@@ -11,6 +11,8 @@ import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSeriali
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+import java.time.Duration;
+
 public class CacheConfig {
     @Autowired
     RedisConnectionFactory redisConnectionFactory;
@@ -25,6 +27,7 @@ public class CacheConfig {
     @Bean
     public CacheManager redisCacheManager() {
         RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofSeconds(3600))
                 .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
                 .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
 
--- backend/src/main/java/com/ssafy/plant/controller/HelloController.java
+++ backend/src/main/java/com/ssafy/plant/controller/HelloController.java
@@ -1,24 +1,16 @@
 package com.ssafy.plant.controller;
 
 import com.ssafy.plant.config.mqtt.MqttConfigSend;
-import com.ssafy.plant.service.BoardService;
-import com.ssafy.plant.dto.Board;
 import com.ssafy.plant.service.PlantService;
-import com.ssafy.plant.service.WeatherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 public class HelloController {
 
     @Autowired
     MqttConfigSend.OutboundGateway outboundGateway;
-
-    @Autowired
-    BoardService boardService;
 
     @Autowired
     PlantService plantService;
@@ -28,18 +20,6 @@ public class HelloController {
 //    mqtt publish test
         outboundGateway.sendToMqtt("test", "STM");
         return "Hello #1";
-    }
-
-//    redis cache test
-    @GetMapping()
-    public List<Board> boards(String size) {
-        List<Board> boards = boardService.getBoards(size);
-        return boards;
-    }
-
-    @GetMapping("count")
-    public int count() {
-        return BoardService.getDbCount();
     }
 
     @GetMapping("nongsaro")
