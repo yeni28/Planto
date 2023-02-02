53d7325e02523abce35a4b63a2297c9e8acfc7eb
Lucas0105 <dnjswo105@gmail.com>
2023-02-02 17:31:24 +0900

    [FE] feat : QR reader를 통해 QR 코드 인식

--- frontend/planto/src/features/home/Main.js
+++ frontend/planto/src/features/home/Main.js
@@ -1,8 +1,11 @@
 import './main.css';
 import backImg from "../../assets/background/backimg.png"
 import BottomNav from '../nav/BottomNav';
+import { useNavigate } from "react-router-dom";
 
 function Main(){
+    const navigate = useNavigate();
+
     const user ={
         name : '황효상', // 이름 DB에서 받아오기
 
@@ -21,7 +24,8 @@ function Main(){
                 <div className="main_back_img">
                 <img src={backImg}  alt="background"className="main_back_img"></img>
                 </div>
-                <button className="main_btn font-PreSB "> + 플랜토 등록하기! </button>
+                
+                <button className="main_btn font-PreSB " onClick={() => {navigate("/enrollment");}}> + 플랜토 등록하기! </button>
             </div>
 
             <BottomNav/>
