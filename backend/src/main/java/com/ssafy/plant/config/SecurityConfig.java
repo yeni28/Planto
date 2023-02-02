d8e237dae4c73be60776ceb31e781ea7b87e25b2
tayo5712 <kimty5712@gmail.com>
2023-02-02 15:43:29 +0900

    [BE] feat : SecurityConfig 생성

---
+++ backend/src/main/java/com/ssafy/plant/config/SecurityConfig.java
@@ -0,0 +1,53 @@
+package com.ssafy.plant.config;
+
+// Oauth 로그인 진행 순서
+// 1. 인가 코드 발급(회원 인증)
+// 2. 엑세스 토큰 발급(접근 권한 부여)
+// 3. 액세스 토큰을 이용해 사용자 정보 불러오기
+// 4. 불러온 사용자 정보를 토대로 자동 회원가입/로그인 진행
+
+import com.ssafy.plant.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final UserRepository userRepository;
+    private final CorsFilter corsFilter;
+    public static final String FRONT_URL = "http://13.125.75.241/";
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .sessionManagement()  // session 을 사용하지 않음
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and()
+                .httpBasic().disable()
+                .formLogin().disable()
+                .addFilter(corsFilter); // @CrossOrigin(인증X), 시큐리티 필터에 등록 인증(O)
+
+        http.authorizeRequests()
+                .antMatchers(FRONT_URL+"/main/**")
+                .authenticated()
+                .anyRequest().permitAll()
+
+                .and()
+                .exceptionHandling();
+//                .authenticationEntryPoint(new CustomAuthenticationEntryPoint());
+
+//        http.addFilterBefore(new JwtRequestFilter(), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+
+    }
+}
