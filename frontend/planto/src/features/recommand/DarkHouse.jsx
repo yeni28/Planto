82c19c644437f93589cec705806945959654909b
yen <suntofu0228@naver.com>
2023-02-08 10:06:56 +0900

    [FE]fix : 식물 등록 페이지 수정

--- frontend/planto/src/features/recommand/DarkHouse.jsx
+++ frontend/planto/src/features/recommand/DarkHouse.jsx
@@ -3,7 +3,6 @@ import React, {useState, useEffect} from 'react'
 import {Link, useNavigate} from 'react-router-dom'
 import BottomNav from '../nav/BottomNav'
 import MoveTopBtn from "../components/MoveTopBtn"
-import "./DarkHouse.css"
 // 이미지
 import TopRecommand from './TopRecommand';
 import BackWhite from '../../assets/icons/back_white.png'
@@ -33,7 +32,7 @@ function Darkhouse() {
       </div>
       <TopRecommand reconame = {reconame}/>
       <div className="RecommandPlant">
-      <div style={{marginTop:"1.2rem"}}>{dplants.map(dplant=>(<DPlant dplant={dplant} key={dplant.plantDictId}/>))}</div>
+      <div>{dplants.map(dplant=>(<DPlant dplant={dplant} key={dplant.plantDictId}/>))}</div>
       </div>
       <div style={{height:'3rem'}}></div>
       <MoveTopBtn/>
@@ -52,14 +51,14 @@ function DPlant({dplant}){
   return (
     <div>
       <Link to={`/dictionary/${dplant.plantDictId}`} state={{plant:dplant, plantName:dplantName, plantNameSecond:dplantNameSecond}}>
-        <div className='plant_line' style={{marginBottom:'1.2rem',}}>
+        <div className='plant_line'>
           <div className="plant_image"> 
             <div className="img_square" style={{
             backgroundImage: `url("https://www.nongsaro.go.kr/cms_contents/301/${dplant.imagePath}")`
             }}>
             </div>
           </div>
-          <div className='plant_text' style={{marginLeft:'0.5rem',marginTop:'0.8rem'}}>
+          <div className='plant_text recommand_plant_text'>
             <p className='font-PreL plant_name' style={{marginBottom:'-0.2rem'}}>{dplantName}</p>  
             <p className='font-PreM plant_name_second' style={{marginBottom:'-0.2rem',color:'#3BD476'}}>{dplantNameSecond}</p>
             <p className='font-PreL plant_name_eng'>{dplant.plantEng}</p>
