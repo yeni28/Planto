f2402e61f375d4a359a4a0f9292ac8051494bf0f
Lucas0105 <dnjswo105@gmail.com>
2023-02-08 15:14:37 +0900

    [BE] : 사용자와 화분 정보를 포함하여 저장

--- backend/src/main/java/com/ssafy/plant/controller/PotController.java
+++ backend/src/main/java/com/ssafy/plant/controller/PotController.java
@@ -1,8 +1,10 @@
 package com.ssafy.plant.controller;
 
 import com.ssafy.plant.domain.PotEntity;
+import com.ssafy.plant.domain.User;
 import com.ssafy.plant.dto.PotDTO;
 import com.ssafy.plant.service.PotService;
+import com.ssafy.plant.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 @RequestMapping("/api/v1/pot")
 public class PotController {
@@ -18,13 +22,13 @@ public class PotController {
     @Autowired
     PotService potService;
 
+    @Autowired
+    UserService userService;
+
     @PostMapping("")
-    public ResponseEntity<String> setPot(@RequestBody PotDTO request){
-        String result = potService.setPot(request);
-        if (result.equals("presented")){
-            return ResponseEntity.status(HttpStatus.OK).body("presented");
-        } else {
-            return ResponseEntity.status(HttpStatus.CREATED).body("created");
-        }
+    public ResponseEntity<String> setPot(HttpServletRequest request, @RequestBody PotDTO potDTO){
+        User user = userService.getUser(request);
+        String result = potService.setPot(potDTO, user);
+        return ResponseEntity.status(HttpStatus.CREATED).body("created");
     }
 }
--- backend/src/main/java/com/ssafy/plant/service/PotService.java
+++ backend/src/main/java/com/ssafy/plant/service/PotService.java
@@ -1,6 +1,7 @@
 package com.ssafy.plant.service;
 
 import com.ssafy.plant.domain.PotEntity;
+import com.ssafy.plant.domain.User;
 import com.ssafy.plant.dto.PotDTO;
 import com.ssafy.plant.repository.PotRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,15 +15,10 @@ public class PotService {
     @Autowired
     PotRepository potRepository;
 
-    public String setPot(PotDTO potDTO){
-        Optional<PotEntity> pot = potRepository.findById(potDTO.getPotId());
-
-        if (pot.isPresent()) {
-            return "presented";
-        } else {
-            PotEntity potEntity = potDTO.dtoToEntity();
-            potRepository.save(potEntity);
-            return "created";
-        }
+    public String setPot(PotDTO potDTO, User user){
+        potDTO.setUser(user);
+        PotEntity potEntity = potDTO.dtoToEntity();
+        potRepository.save(potEntity);
+        return "created";
     }
 }
