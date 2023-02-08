095a54a0c40eb27c777f111da069151e0475ab24
yen <suntofu0228@naver.com>
2023-02-08 10:30:02 +0900

    [FE]feat : 소셜 로그인 구현

--- frontend/planto/src/features/login/KakaoLogin.js
+++ frontend/planto/src/features/login/KakaoLogin.js
@@ -1,9 +1,11 @@
 import React, { useEffect } from 'react'
 // import { useNavigate } from 'react-router-dom';
-// import {CLIENT_ID , REDIRECT_URI} from "./OAuth";
+import {HOST} from "./OAuth";
 import axios from 'axios'
+import { useNavigate } from 'react-router-dom';
 function KakaoLogin() {
     
+    const navigate = useNavigate();
     // TOKEN저장
     
     useEffect(() => {
@@ -12,7 +14,7 @@ function KakaoLogin() {
         const KAKAO_CODE = PARAMS.get('code');
     
         console.log({KAKAO_CODE})
-        const backHost = `http://localhost:8080/api/v1/oauth/token?code=${KAKAO_CODE}`
+        const backHost = `${HOST}/api/v1/oauth/token?code=${KAKAO_CODE}`
         await axios({
 
             url: backHost,
@@ -24,29 +26,17 @@ function KakaoLogin() {
             window.localStorage.setItem('token', token);
             // 로컬스토리지에 토큰이 있으면 메인페이지로 이동
             // 없으면 로그인 페이지로 리다이렉트
-            // if (token) {
-            //     navigate('/main/none')
-            // } else {
-            //     navigate('/')
-            // }
+            if (token) {
+                navigate('/main/none')
+            } else {
+                navigate('/')
+            }
         })
         .catch((err) => {
             console.log(err)
         })
         
-        const token = window.localStorage.getItem('token');
-        try {
-            // 차후 백에서 api받아서 수정
-            const res =await axios.post('/api/post', KAKAO_CODE,{
-                headers:{
-                    Authorization:KAKAO_CODE,
-                },
-            }).then((res) => {
-                console.log(res);
-            });
-        } catch(e){
-            console.error(e)
-        }
+ 
     })();
     }, [])
 
