d709fa331734e8812e3daffc99bb31e5ac8f9946
Lucas0105 <dnjswo105@gmail.com>
2023-02-13 15:52:11 +0900

    [BE] feat : 업적 달성 기능 추가

--- backend/src/main/java/com/ssafy/plant/service/MachinePlantService.java
+++ backend/src/main/java/com/ssafy/plant/service/MachinePlantService.java
@@ -2,14 +2,16 @@ package com.ssafy.plant.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.ssafy.plant.domain.Plant;
-import com.ssafy.plant.domain.PotEntity;
+import com.ssafy.plant.domain.*;
+import com.ssafy.plant.repository.AchievementRepository;
 import com.ssafy.plant.repository.PlantRepository;
 import com.ssafy.plant.repository.PotRepository;
+import com.ssafy.plant.repository.UserAchievementRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
@@ -23,6 +25,12 @@ public class MachinePlantService {
     PotRepository potRepository;
 
     @Autowired
+    AchievementRepository achievementRepository;
+
+    @Autowired
+    UserAchievementRepository userAchievementRepository;
+
+    @Autowired
     LikingService likingService;
 
     public void getPlant(Object payload) throws JsonProcessingException {
@@ -34,22 +42,12 @@ public class MachinePlantService {
         int touch_status = Integer.parseInt(map.get("touch"));
         int attack = Integer.parseInt(map.get("attack"));
 
-        PotEntity pot = potRepository.findByPotId(1234L);
-        System.out.println("================================");
-        try{
-            System.out.println(pot.getClass().getName());
-            System.out.println(pot);
-        } catch(Exception e) {
-            e.printStackTrace();
-            System.out.println(e);
-        }
-        System.out.println("================================");
+        PotEntity pot = potRepository.findByPotId(serialNo);
 
         Plant plant = pot.getPlant();
         int touch = plant.getTouch();
         attack += plant.getAttack();
         int liking = plant.getLiking();
-
 
         if (touch_status == 1){
             touch += 1;
@@ -66,6 +64,37 @@ public class MachinePlantService {
             liking -= 1;
             checkLiking(pot.getPotId(), liking);
         }
+
+        User user = pot.getUser();
+
+        List<UserAchievementEntity> userAchievementEntity = userAchievementRepository.findByUser(user);
+
+        // 1번 웃는 플랜토
+        if (touch >= 15){
+            saveAchievement(user, userAchievementEntity, 1);
+        }
+        // 2번 플렌토가 화가 났습니다.
+        if (liking <= 30){
+            saveAchievement(user, userAchievementEntity, 2);
+        }
+        // 3번 플랜토의 이마에 혹이 생겼습니다.
+        if (attack >= 50){
+            saveAchievement(user, userAchievementEntity, 3);
+        }
+        // 4번 플랜토는.... 귀엽습니다
+        if (touch >= 100){
+            saveAchievement(user, userAchievementEntity, 4);
+        }
+        // 5번 플랜토의 볼에 구멍이 뚫릴것같습니다.
+        if (attack >= 100){
+            saveAchievement(user, userAchievementEntity, 5);
+        }
+
+        // 9번 플랜토가 행복해졌습니다
+        if (liking >= 70){
+            saveAchievement(user, userAchievementEntity, 9);
+        }
+
 
         plant.setTemperature((int)Double.parseDouble(map.get("temperature")));
         plant.setHumidity((int)Double.parseDouble(map.get("humidity")));
@@ -92,5 +121,19 @@ public class MachinePlantService {
         } else{
             likingService.sendLiking(potId, 2);
         }
+    }
+
+    public void saveAchievement(User user, List<UserAchievementEntity> userAchievementEntity, int achievement_id){
+        for (UserAchievementEntity userAchievement : userAchievementEntity){
+            if (userAchievement.getAchievement().getAchievement_id() == achievement_id){
+                return;
+            }
+        }
+
+        AchievementEntity achievement = achievementRepository.findById(achievement_id).get();
+
+        UserAchievementEntity userAchievement = UserAchievementEntity.builder().user(user).achievement(achievement).build();
+        userAchievementRepository.save(userAchievement);
+
     }
 }
