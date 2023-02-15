1266513e3ead536961f895bd8fdffce367d2e5fd
yen <suntofu0228@naver.com>
2023-02-15 17:49:25 +0900

    [FE]fix : 반응형 웹 페이지 생성

--- frontend/planto/src/features/home/App.js
+++ frontend/planto/src/features/home/App.js
@@ -1,31 +1,41 @@
-import './App.module.css';
+import './App.css';
 import Login from '../login/Login';
 import Back from '../../assets/background/appback.jpg'
 import Logo from '../../assets/icons/logo.png'
 function App() {
 
   return ( 
-  
-  <div className='App'>
-    <div className="appback"style={{width:'100vw',height:'100vh',
-    paddingTop:'10rem', backgroundImage:`url("${Back}")`,backgroundSize:'cover',textAlign: ' center'
-    }}>      
-      <div>
-        <img src={Logo} alt="logo"
-        style={{
-          width:'3rem',
-          margin:'auto',
-          marginBottom:'1rem',
-        }}></img>
-        <div className='font-PreM apptext ' style={{marginBottom:'1.5rem'}}>
-          <p style={{fontSize:'1.5rem',fontFamily: 'RIDIBatang'}}> 마음을 나누는 </p>
-          <p style={{fontSize:'1.5rem',fontFamily: 'RIDIBatang'}}> 나만의 반려식물, 플랜토 </p>
-          
+  <div>
+
+    {/* 모바일 */}
+    <div className="mobile">
+      <div className='App'>
+        <div className="appback"style={{width:'100vw',height:'100vh',
+        paddingTop:'10rem', backgroundImage:`url("${Back}")`,backgroundSize:'cover',textAlign: ' center'
+        }}>      
+          <div>
+            <img src={Logo} alt="logo"
+            style={{
+              width:'3rem',
+              margin:'auto',
+              marginBottom:'1rem',
+            }}></img>
+            <div className='font-PreM apptext ' style={{marginBottom:'1.5rem'}}>
+              <p style={{fontSize:'1.5rem',fontFamily: 'RIDIBatang'}}> 마음을 나누는 </p>
+              <p style={{fontSize:'1.5rem',fontFamily: 'RIDIBatang'}}> 나만의 반려식물, 플랜토 </p>
+              
+            </div>
+            <Login/>
+          </div>
         </div>
-        <Login/>
       </div>
-     </div>
+    </div>
+    {/* 웹 */}
+    <div className="web">
+      
+    </div>
   </div>
+
   );
   
 }
